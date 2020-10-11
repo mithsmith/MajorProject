51cadb292590bf9a9c25308e37395c0e4db18db5
Smrithi Shankar <54745805+mithsmith@users.noreply.github.com>
2020-10-12 01:12:50 +0530

    Add files via upload
    First commit

---
+++ app1.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+from sklearn.naive_bayes import MultinomialNB
+from PIL import Image
+
+SAVED_MODEL_FILE = "SavedModel.sav"
+ 
+#df = pd.read_csv('senti_review_file.csv')
+#x = df.iloc[:,0].values # Review column as input
+#y = df.iloc[:,1].values # Sentiment column as output
+st.title("Sentiment Analysis On Review")
+st.subheader('TFI-DF Vectorizer')
+st.write('This project is based on Naive Bayes Classifier.')
+loaded_model = joblib.load(SAVED_MODEL_FILE)
+#text_model = Pipeline([('tfidf',TfidfVectorizer()),('model',MultinomialNB())])
+#text_model.fit(x,y)
+message = st.text_area("Enter Text","Type Here ..")
+out_text = loaded_model.predict([message])
+if st.button("Analyze Sentiment"):
+  st.title(out_text)
