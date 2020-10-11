d45bcaeb8244bd06ccb838a14d5c1fe85ca5e8df
Smrithi Shankar <54745805+mithsmith@users.noreply.github.com>
2020-10-12 03:08:02 +0530

    Add files via upload
    CRLF to LF (Unix mode)

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"humannjusta@gmail.com\"\n\
+email=\"humannjusta@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 
 echo "\
 [server]\n\
-headless = true\n\
+headless=true\n\
 enableCORS=false\n\
-port = $PORT\n\
+port=$PORT\n\
 " > ~/.streamlit/config.toml
